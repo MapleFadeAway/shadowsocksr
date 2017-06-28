d86b8ec1ab40d9dcd53936b9f4e041e49d6713ee
MapleFadeAway <forer@bupt.edu.cn>
2017-06-28 11:13:48 +0800

    Add MDB

--- server.py
+++ server.py
@@ -50,6 +50,8 @@ def main():
 			thread = MainThread(db_transfer.MuJsonTransfer)
 		elif get_config().API_INTERFACE == 'sspanelv2':
 			thread = MainThread(db_transfer.DbTransfer)
+		elif get_config().API_INTERFACE == 'mdb':
+			thread = MainThread(db_transfer.MDbTransfer)
 		else:
 			thread = MainThread(db_transfer.Dbv3Transfer)
 		thread.start()
